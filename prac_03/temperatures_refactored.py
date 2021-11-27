50c2fd955ca9d5d4406cb52ce36a83158c1ce113
Richard Reynard <richard.reynard@my.jcu.edu.au>
2021-11-27 22:34:01 +0800

    Copy temperatures to prac 3 and commit before refactoring.

---
+++ prac_03/temperatures_refactored.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion refactored
+Created on 27-11-21 by Richard Reynard
+"""
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        user_celsius = float(input("Celsius: "))
+        fahrenheit = user_celsius * 9.0 / 5 + 32
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        user_fahrenheit = float(input("Fahrenheit: "))
+        celsius = 5 / 9 * (user_fahrenheit - 32)
+        print("Result: {:.2f} F".format(celsius))
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
