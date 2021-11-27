cf5f4ccfd8b503e020ff8c8042688f95d724baad
Richard Reynard <richard.reynard@my.jcu.edu.au>
2021-11-27 22:58:11 +0800

    Refactored to use functions to determine grades.

--- prac_03/broken_score_refactored.py
+++ prac_03/broken_score_refactored.py
@@ -1,14 +1,26 @@
 """
 CP1404/CP5632 - Practical
 Broken program to determine score status
+Created on 27-11-21 by Richard Reynard
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+
+def main():
+    """Get score from user and determine the result"""
+    score = float(input("Enter score: "))
+    print(determine_grades(score))
+
+
+def determine_grades(score):
+    """Determine grade based on score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+main()
