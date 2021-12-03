2de02f3d20e07dc430398de9f64ec58181c949f1
Richard Reynard <richard.reynard@my.jcu.edu.au>
2021-12-03 11:59:07 +0800

    Update main docstring.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,6 +7,7 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Print subject with correct formatting"""
     user_subject = get_subjects()
     print(user_subject)
     for subject in user_subject:
@@ -18,13 +19,13 @@ def get_subjects():
     input_file = open(FILENAME)
     subjects = []
     for line in input_file:
-        # print(line)  # See what a line looks like
-        # print(repr(line))  # See what a line really looks like
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        # print(parts)  # See what the parts look like (notice the integer is a string)
+        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        # print(parts)  # See if that worked
+        print(parts)  # See if that worked
         subjects.append(parts)
     input_file.close()
     return subjects
