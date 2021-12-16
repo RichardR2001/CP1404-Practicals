48806b27adc17bbad1898a1a8c2f0bd9661a456e
Richard Reynard <richard.reynard@my.jcu.edu.au>
2021-12-16 13:51:27 +0800

    Add user code and loop.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -9,4 +9,7 @@ COLOUR_CODES = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue"
                 "AntiqueWhite": "#faebd7", "Bleu de France": "#318ce7", "Blond": "#faf0be", "Blue (Pantone)": "#0018a8",
                 "Brilliant Rose": "#ff55a3", "British Racing Green": "#004225"}
 
-
+user_colour = str(input("Enter your choice of colour: ")).title()
+while user_colour != "":
+    print(f"The code for color {user_colour} is {COLOUR_CODES.get(user_colour)}")
+    user_colour = str(input("Enter your choice of colour: ")).title()
