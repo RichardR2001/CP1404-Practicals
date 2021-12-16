56b67d0c9601ff47843a2a48537f1129d89d9d73
Richard Reynard <richard.reynard@my.jcu.edu.au>
2021-12-16 13:50:19 +0800

    Add dictionary to file.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+"""
+CP1404 Practical 5
+Hexadecimal Colour Codes
+Created on 16-12-2021 by Richard Reynard
+"""
+
+COLOUR_CODES = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue" : "#f0f8ff",
+                "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "ffbf00", "Amethyst": "#9966cc",
+                "AntiqueWhite": "#faebd7", "Bleu de France": "#318ce7", "Blond": "#faf0be", "Blue (Pantone)": "#0018a8",
+                "Brilliant Rose": "#ff55a3", "British Racing Green": "#004225"}
+
+
