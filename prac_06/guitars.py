92ce2d20fa060d5311cf60ac4fbb563eee866675
Richard Reynard <richard.reynard@my.jcu.edu.au>
2021-12-24 23:48:59 +0800

    Add loop to print all the owned guitars.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -22,5 +22,9 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+    print("I have these guitars:")
+    for i, guitar in enumerate(guitars):
+        print(f"{guitar.name:20}, Price: {guitar.cost:6} from year {guitar.year} ")
+
 
 main()
