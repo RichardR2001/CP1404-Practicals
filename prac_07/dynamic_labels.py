9b26eb6b4404505ec8c70f150c44273b9ab35b67
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-01-02 23:55:47 +0800

    Add docstring to functions.

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -11,19 +11,23 @@ from kivy.properties import StringProperty
 
 
 class DynamicLabelsApp(App):
+    """Main App - Kivy app to demo dynamic label creation."""
     status_text = StringProperty()
 
     def __init__(self, **kwargs):
+        """Construct main app."""
         super().__init__(**kwargs)
         self.name_list = ['Alexander', 'Daryl', 'Jessica', 'Riley']
 
     def build(self):
+        """Build the Kivy GUI."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
         self.create_labels()
         return self.root
 
     def create_labels(self):
+        """Create labels from list of names and add them to the GUI."""
         for name in self.name_list:
             label_name = Label(text=name)
             self.root.ids.main.add_widget(label_name)
