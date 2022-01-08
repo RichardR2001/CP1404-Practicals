648068d71fa3888af45ff4938fea2b9318aa6a64
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-01-09 00:33:25 +0800

    Add main function to test SilverServiceTaxi.

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,17 @@
+"""
+CP1404 - Practical
+Silver Service Taxi
+"""
+
+from CP1404.prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """..."""
+    taxi = SilverServiceTaxi("Hummer", 100, 4)
+    taxi.drive(20)
+    print(taxi)
+    print(f"Total fare: ${taxi.get_fare()}")
+
+
+main()
