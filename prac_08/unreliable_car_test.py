48c0a0f03a390e404e74327074134b1fb0f08ec6
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-01-08 23:12:20 +0800

    Add main function.

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,19 @@
+"""
+CP1404 - Programming 2
+Unreliable Car Test
+Created on 07-01-2022 by Richard Reynard
+"""
+
+from unreliable_car import UnreliableCar
+
+
+def main():
+    """..."""
+
+    reliable_car = UnreliableCar("Car 1", 100, 99)
+    unreliable_car = UnreliableCar("Car 2", 100, 1)
+
+
+
+
+main()
