381e6c3dff364672039674c4499e19f5fc51ab72
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-01-18 20:43:25 +0800

    Add command to ask user input and assign file to folder based on input.

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -13,7 +13,21 @@ def main():
         if os.path.isdir(filename):
             continue
 
+        ext = get_extension(filename)
+        if ext not in ext_type_to_cat:
+            category = input("What category would you like to sort {} files into? ".format(ext))
+            ext_type_to_cat[ext] = category
+            try:
+                os.mkdir(category)
+            except FileExistsError:
+                pass
 
+        os.rename(filename, "{}/{}".format(ext_type_to_cat[ext], filename))
+
+
+def get_extension(filename):
+    ext = filename.split('.')[-1]
+    return ext
 
 
 main()
