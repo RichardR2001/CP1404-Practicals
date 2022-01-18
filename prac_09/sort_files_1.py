c2fe27298709563b6b23fd458eaee9ee39f91817
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-01-18 20:32:14 +0800

    Add loop to move the files.

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -11,7 +11,18 @@ def main():
     print(f"Sorting files in: {FILE_DESTINATION}")
     for directory_name, subdirectory, filename in os.walk('.'):
         type_list = create_type_list(filename)
-
+        for ext_type in type_list:
+            try:
+                os.mkdir(ext_type)
+            except FileExistsError:
+                pass
+        try:
+            for file in filename:
+                file_ext = get_extension(file)
+                shutil.move(file, file_ext)
+        except shutil.Error:
+            pass
+    print("Sort complete")
 
 
 def create_type_list(filenames):
